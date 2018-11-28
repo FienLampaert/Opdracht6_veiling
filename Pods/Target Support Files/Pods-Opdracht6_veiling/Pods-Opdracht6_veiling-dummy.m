90c578db4aed74ae8aa25473e48afbd2d8b21e61
student <student@gebruikers-mac-mini-2.local>
2018-11-28 10:44:53 +0100

    commit

---
+++ Pods/Target Support Files/Pods-Opdracht6_veiling/Pods-Opdracht6_veiling-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Opdracht6_veiling : NSObject
+@end
+@implementation PodsDummy_Pods_Opdracht6_veiling
+@end
---
+++ Pods/Target Support Files/Pods-Opdracht6_veiling/Pods-Opdracht6_veiling-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Opdracht6_veilingVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Opdracht6_veilingVersionString[];
+
