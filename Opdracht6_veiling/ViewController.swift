f17dc5c372b5d87f56fad255677ca02af626dcb3
student <student@gebruikers-mac-mini-2.local>
2018-11-28 08:54:33 +0100

    Initial Commit

---
+++ Opdracht6_veiling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Opdracht6_veiling
+//
+//  Created by student on 28/11/2018.
+//  Copyright © 2018 Vives. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
