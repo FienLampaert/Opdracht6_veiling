2b877918f8a95359ce3f0cdcf3b88a4a7f7aca7a
student <student@gebruikers-mac-mini-2.local>
2018-11-29 10:47:18 +0100

    register screen

---
+++ Opdracht6_veiling/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  Opdracht6_veiling
+//
+//  Created by student on 29/11/2018.
+//  Copyright © 2018 Vives. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
