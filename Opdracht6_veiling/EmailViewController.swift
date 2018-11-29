f3c55852df882a75493b7cee130ae1dabfc920cf
student <student@gebruikers-mac-mini-2.local>
2018-11-29 10:28:56 +0100

    FIRST SCREEN

--- Opdracht6_veiling/EmailViewController.swift
+++ Opdracht6_veiling/EmailViewController.swift
@@ -11,6 +11,10 @@ import FirebaseAuth
 
 class EmailViewController: UIViewController {
 
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var lbTest: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,10 +24,31 @@ class EmailViewController: UIViewController {
 
     func createUser(email: String, password: String){
         Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
-            // ...
+            print("error " + error.debugDescription)
+            print("authResult " + authResult.debugDescription)
             guard let user = authResult?.user else { return }
         }
     }
     
-
+    func signIn(email:String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            print("error" + error.debugDescription)
+            print("user" + user.debugDescription)
+        }
+    }
+    
+    @IBAction func clickRegisterBtn(_ sender: Any) {
+        
+        if let email = txtEmail.text ,let password = txtPassword.text {
+            createUser(email: email, password: password)
+            lbTest.text = "JAAAAAA"
+        }
+        
+//        if let email = txtEmail.text ,let password = txtPassword.text {
+//            signIn(email: email, password: password)
+//            lbTest.text = "JAAAAAA"
+//        }
+        
+    }
+    
 }
